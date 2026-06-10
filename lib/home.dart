1ab748b47ed9734c36cdb172d04bb370cef9c59e
sharmakartik86000-code <sharmakartik86000@gmail.com>
2026-06-10 11:16:27 +0530

    Search Bar Add

--- lib/home.dart
+++ lib/home.dart
@@ -1,33 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
-  
+  const Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController searchcontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Stack(
-       children: [
-        Container(
-          decoration: BoxDecoration( 
-            gradient: LinearGradient(
-            colors: [
-              Color(0xff213A50),
-              Color(0xff071938),
-               
-              
-            ]
-           ),
-        )
-        )
-       ],
-      ),
+        children: [
+          // Background
+          Container(
+            width: double.infinity,
+            height: double.infinity,
 
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+
+                colors: [Color(0xff213A50), Color(0xff071938)],
+              ),
+            ),
+          ),
+
+          // Search Wala Container FOr Searching Rwcepie stttttt
+          Column(
+            children: [
+              SafeArea(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  margin: EdgeInsets.fromLTRB(20, 25, 20, 15),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withValues(alpha: 0.95),
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 15,
+                        spreadRadius: 2,
+                        offset: Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          if (searchcontroller.text.trim().isEmpty) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text("Please Enter City Name")),
+                            );
+                          } else {
+                            Navigator.pushReplacementNamed(
+                              context,
+                              "/loading",
+                              arguments: {"searchText": searchcontroller.text},
+                            );
+                          }
+                        },
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                          child: Icon(
+                            Icons.search_rounded,
+                            color: Colors.blue,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+
+                      Expanded(
+                        child: TextField(
+                          controller: searchcontroller,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Search  Let's Cook Something...",
+                            hintStyle: TextStyle(
+                              color: const Color.fromARGB(255, 93, 93, 93),
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  children: [
+                    Text(
+                      " App Ko ky chahiye Kaane Me?  ",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Lets Cook Something New... ",
+                      style: GoogleFonts.exo2(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          // Search Wala Container FOr Searching Rwcepie Enddddd
+
+          // Main UI
+        ],
+      ),
     );
   }
 }
